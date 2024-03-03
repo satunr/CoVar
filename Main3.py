4a5026e06c156418606666d6c663d14d638aacb4
satunr <satyakir@unc.edu>
2024-03-03 01:50:56 -0600

    Main3.py

--- Main3.py
+++ Main3.py
@@ -8,7 +8,7 @@ import pickle
 import math
 import pandas as pd
 
-# from networkx.algorithms.community.centrality import girvan_newman
+from networkx.algorithms.community.centrality import girvan_newman
 from Nearest import *
 from variation import *
 from scipy import stats
@@ -73,35 +73,35 @@ def main(ind):
     print (extract)
 
     data = data[extract, :]
+    print ('Expression matrix has shape (%d samples, %d genes)' % (data.shape))
 
-    # data, g_names = remove_noncoding(data, g_names)
-    # print (len(g_names))
+    data, g_names = remove_noncoding(data, g_names)
+    print (len(g_names))
 
-    # Find TPM
+    # # Find TPM
     # lengths = read_annotations('annotationMatrix2.csv')
     # data = find_tpm(lengths, data, g_names)
     # exit(1)
 
-    # Find gene symbols
-    m, g_names = mappings(g_names)
-    pickle.dump([g_names, m], open('m.p', 'wb'))
+    # # Find gene symbols
+    # m, g_names = mappings(g_names)
+    # pickle.dump([g_names, m], open('m.p', 'wb'))
 
     [g_names, m] = pickle.load(open('m.p', 'rb'))
     g_names = [m[gene] for gene in g_names]
-    # check(g_names)
+    print ('There are %d shortlisted genes.' % (len(g_names)))
 
     # STEP 2: Eliminate lowly expressed genes
     # D = None
     D = pickle.load(open('M-values.p', 'rb'))
-    # data, g_names, D = eliminate(data, g_names, cut_off, cv_cutoff, D)
+    data, g_names, D = eliminate(data, g_names, cut_off, cv_cutoff, D)
     pickle.dump([data, g_names], open('Y.p', 'wb'))
     print (len(g_names))
 
     [data, g_names] = pickle.load(open('Y.p', 'rb'))
     print ('Number of genes left. ', len(g_names))
     print (len(g_names), np.shape(data))
-
-    # check_presence(g_names, ['DEFA5', 'DEFA6', 'REG1A', 'REG3A', 'CXCL5'])
+    # exit(1)
 
     # STEP 3: Normalize dataset
     data[-1, :] += 0.01
@@ -116,35 +116,35 @@ def main(ind):
     GList = []
     con, dis = None, None
     for i in range(2):
-
         S[0].append(g_names)
         S[1].append(data[X[i]: X[i + 1], :])
 
         # STEP 4a: Create genie matrix and isolate gene names
         VIM3 = GENIE3(data[X[i]: X[i + 1], :], nthreads = nth, tree_method=tree_method, ntrees = ntrees)
         VIM3 = VIM3/np.max(VIM3)
+        print (np.shape(VIM3))
+
         GList.append(VIM3)
 
         # STEP 4b: Create genie graph
         G = create_graph(VIM3, g_names)
-        print (len(G), len(G.edges()))
+        print (len(G))
 
         S[2].append([G, VIM3])
 
         if i == 0:
-            con = deepcopy(VIM3)
+            con = deepcopy(G)
         elif i == 1:
-            dis = deepcopy(VIM3)
+            dis = deepcopy(G)
             GList.append(G)
 
-    vg, GList = find_vars2(GList, con, dis, V, g_names)
-    print ('Variational nodes.', vg)
+    vg, GList = find_vars(GList, con, dis, V)
+    print ('Variational nodes are:', vg)
     GList.append(vg)
 
     # STEP 5: Find k-nearest neighbor graph on disease graph
-    knn = nearest2(GList[2], vg, K, approach)
-    print (len(knn), len(knn.edges()))
-    # exit(1)
+    knn = nearest2(dis, vg, K, approach)
+    print ('The nearest neighbor network has %d nodes and %d edges', (len(knn), len(knn.edges())))
 
     GList.append(knn)
 
@@ -162,14 +162,13 @@ def main(ind):
             c = deepcopy(list(nx.k_core(create(knn, subset)).nodes()))
         Cr.append(c)
 
-    print ('Communities.', [Cr[i] for i in range(len(Cr))])
+    print ('Module-wise genes.', [Cr[i] for i in range(len(Cr))])
 
     GList.append(Cr)
-    pickle.dump(GList, open('Run2-' + str(ind) + '.p', 'wb'))
-    # exit(1)
+    pickle.dump(GList, open('GList-Macro-a-' + str(ind) + '.p', 'wb'))
 
 
-for ind in range(how_many_runs):
+for ind in range(20, how_many_runs):
     # Each run
     print ('Run.', ind)
     main(ind)
